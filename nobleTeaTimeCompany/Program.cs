acd1d38e0960cef3587c72cb364102ec99d1f6c1
Eliseo <Eliseo@LAPTOP-OIA8MLF7>
2023-10-23 08:45:30 +0700

    Add project files.

---
+++ nobleTeaTimeCompany/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        const int shippingFee = 15;
+
+        Console.Write("Enter no. of pound: ");
+        int noOfPound = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter price per pound: ");
+        double pricePerPound = double.Parse(Console.ReadLine());
+
+        Console.Write("Add shipping fee (Y/N): ");
+        char shippingInformation = Console.ReadLine()[0];
+
+        double totalAmount = noOfPound * pricePerPound;
+
+        if (shippingInformation == 'Y') ;
+        {
+            totalAmount = totalAmount + shippingFee;
+        }
+
+        Console.WriteLine("Total amount: {0:F2}", totalAmount);
+    }
+}
